118115399a5d5ff90950be2e8b0b51ea72c9837b
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-01-12 22:10:01 +0800

    Edit the display message

--- cli.py
+++ cli.py
@@ -5,7 +5,7 @@ app = typer.Typer()
 # Cluster operations
 @app.command(short_help="Register an existing cluster")
 def add_cluster(name: str, ring: int, config_file: str):
-    typer.echo(f"Adding Cluster {str}")
+    typer.echo(f"Adding Cluster {name} on Ring {ring}")
 
 if __name__ == "__main__":
     app() 

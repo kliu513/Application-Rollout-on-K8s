77a09e47dcf047f67c88bcc9e50eb1d02e34493b
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-01-12 22:02:48 +0800

    Add add_cluster()

---
+++ cli.py
@@ -0,0 +1,11 @@
+import typer
+
+app = typer.Typer()
+
+# Cluster operations
+@app.command(short_help="Register an existing cluster")
+def add_cluster(name: str, ring: int, config_file: str):
+    typer.echo(f"Adding Cluster {str}")
+
+if __name__ == "__main__":
+    app() 

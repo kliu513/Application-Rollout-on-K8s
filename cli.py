72234dff6a749890f640a693ef6008ce5141f34d
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-01-20 00:05:27 +0800

    Test

--- cli.py
+++ cli.py
@@ -46,7 +46,7 @@ def display_clusters():
     for cluster in clusters: 
         table.add_row(cluster.name, str(cluster.ring), cluster.config, cluster.timestamp)
     console.print(table)
-    subprocess.run(['scripts/add-cluster.sh','argument'], shell=True)
+    print(subprocess.run(['scripts/add-cluster.sh','argument'], shell=True))
 
 def build_cluster_table():
     table = Table(show_header=True, header_style="blue")

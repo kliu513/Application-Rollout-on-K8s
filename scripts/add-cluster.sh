f1a366b5ced4ddce8d06e1dafc8793db5b69111e
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-02-02 14:56:06 +0800

    Test

--- scripts/add-cluster.sh
+++ scripts/add-cluster.sh
@@ -3,6 +3,7 @@ touch log.txt
 kubectl --kubeconfig config-files/fleet-manager.cfg use-context fleet-manager
 kubectl -n clusters get clusters.fleet.cattle.io >> log.txt
 n_lines_before=`wc --lines < log.txt`
+echo "$n_lines_before"
 kubectl --kubeconfig $1 use-context fleet-manager
 helm -n cattle-fleet-system install --create-namespace --wait \
     --set-string labels.group=$2 \
@@ -11,8 +12,10 @@ helm -n cattle-fleet-system install --create-namespace --wait \
 kubectl --kubeconfig config-files/fleet-manager.cfg use-context fleet-manager
 kubectl -n clusters get clusters.fleet.cattle.io > log.txt
 n_lines_after=`wc --lines < log.txt`
+echo "$n_lines_after"
 rm log.txt
-if [$n_lines_after -gt $n_lines_before]; then
+if [$n_lines_after -gt $n_lines_before]
+then
     exit 0
 else
     exit 1

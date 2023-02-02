ab326f9a5ec300a0d3d9f89b0d20241007480aae
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-02-02 14:51:20 +0800

    Fix

--- scripts/add-cluster.sh
+++ scripts/add-cluster.sh
@@ -9,10 +9,10 @@ helm -n cattle-fleet-system install --create-namespace --wait \
     --values ../config-files/values.yaml \
     fleet-agent https://github.com/rancher/fleet/releases/download/v0.5.0/fleet-agent-0.5.0.tgz
 kubectl --kubeconfig config-files/fleet-manager.cfg use-context fleet-manager
-kubectl -n clusters get clusters.fleet.cattle.io >> log.txt
+kubectl -n clusters get clusters.fleet.cattle.io > log.txt
 n_lines_after=`wc --lines < log.txt`
 rm log.txt
-if [$n_lines_after > $n_lines_before]; then
+if [$n_lines_after -gt $n_lines_before]; then
     exit 0
 else
     exit 1

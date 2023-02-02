39409d9fb4b8a524a4f0bfde64b797b6758b16a4
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-02-02 14:04:57 +0800

    Edit add-cluster.sh

--- scripts/add-cluster.sh
+++ scripts/add-cluster.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 touch log.txt
-kubectl config use-context fleet-manager
-kubectl --namespace clusters get fleet >> log.txt
+kubectl --kubeconfig ../config-files/fleet-manager.cfg use-context fleet-manager
+kubectl -n clusters get clusters.fleet.cattle.io >> log.txt
 n_lines_before=`wc --lines < log.txt`
+kubectl --kubeconfig $1 use-context fleet-manager
 helm -n cattle-fleet-system install --create-namespace --wait \
-    --kubeconfig $1 \
     --set-string labels.group=$2 \
-    --values values.yaml \
+    --values ../config-files/values.yaml \
     fleet-agent https://github.com/rancher/fleet/releases/download/v0.5.0/fleet-agent-0.5.0.tgz
-kubectl --namespace clusters get fleet > log.txt
+kubectl --kubeconfig ../config-files/fleet-manager.cfg use-context fleet-manager
+kubectl -n clusters get clusters.fleet.cattle.io >> log.txt
 n_lines_after=`wc --lines < log.txt`
 rm log.txt
 if [$n_lines_after > $n_lines_before]; then

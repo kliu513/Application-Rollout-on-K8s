1b73207f34dbb7ec5ec9111cd353a31d75bf7237
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-02-02 16:20:39 +0800

    Test

--- scripts/add-cluster.sh
+++ scripts/add-cluster.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
+export KUBECONFIG_MAN="config-files/fleet-manager.cfg"
 touch log.txt
-kubectl --kubeconfig config-files/fleet-manager.cfg config use-context fleet-manager 
-kubectl -n clusters get clusters.fleet.cattle.io >> log.txt
+kubectl --kubeconfig $KUBECONFIG_MAN -n clusters get clusters.fleet.cattle.io >> log.txt
 n_lines_before=`wc --lines < log.txt`
-kubectl --kubeconfig $3 config use-context $1
 helm -n cattle-fleet-system install --create-namespace --wait \
-    --set-string labels.group=$2 \
+    --kubeconfig $2
+    --set-string labels.group=$1 \
     --values config-files/values.yaml \
     fleet-agent https://github.com/rancher/fleet/releases/download/v0.5.0/fleet-agent-0.5.0.tgz
-kubectl --kubeconfig config-files/fleet-manager.cfg config use-context fleet-manager
-kubectl -n clusters get clusters.fleet.cattle.io > log.txt
+kubectl --kubeconfig $KUBECONFIG_MAN -n clusters get clusters.fleet.cattle.io > log.txt
 n_lines_after=`wc --lines < log.txt`
 rm log.txt
 if [$n_lines_after -gt $n_lines_before]

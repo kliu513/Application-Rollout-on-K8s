6e617c66c3bb1be3e9ed281742211678b15fd9c7
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-02-09 23:53:23 +0800

    Test

--- scripts/update-cluster-ring.sh
+++ scripts/update-cluster-ring.sh
@@ -9,10 +9,13 @@ IFS=' ' read -ra CLUSTER <<< "$(grep $NODE log.txt)"
 export CLUSTER
 rm clusters.txt
 touch cluster.json
+echo "here"
 kubectl --kubeconfig $KUBECONFIG_MAN -n clusters get clusters.fleet.cattle.io $CLUSTER -o json > cluster.json
+echo "here"
 sed -i.bak "s/$2/$3/g" cluster.json
 kubectl apply -f cluster.json
 kubectl --kubeconfig $KUBECONFIG_MAN -n clusters get clusters.fleet.cattle.io $CLUSTER -o json > cluster.json
+echo "here"
 if [ grep $2 cluster.json ]
 then
     rm cluster.json

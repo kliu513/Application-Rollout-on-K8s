98764017962287c00fe5e8f242d3cd5517685064
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-02-09 23:03:53 +0800

    Test

--- scripts/update-cluster-ring.sh
+++ scripts/update-cluster-ring.sh
@@ -8,6 +8,7 @@ kubectl --kubeconfig $KUBECONFIG_MAN get clusters -n clusters > log.txt
 echo "here"
 export NODE=$(sed -e 's/^"//' -e 's/"$//' <<< "$NODE")
 export CLUSTER=$(grep $NODE log.txt)
+echo $CLUSTER
 rm clusters.txt
 touch cluster.json
 kubectl --kubeconfig $KUBECONFIG_MAN -n clusters get cluster $CLUSTER -o json > cluster.json

65d282eaa7ca17c9e255a84e0d0c79868a4cac11
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-02-10 00:57:43 +0800

    Edit remove-cluster.sh

--- scripts/remove-cluster.sh
+++ scripts/remove-cluster.sh
@@ -2,6 +2,16 @@
 touch log.txt
 helm --kubeconfig $1 -n cattle-fleet-system uninstall fleet-agent
 kubectl --kubeconfig $1 get deployments --all-namespaces > log.txt
+export KUBECONFIG_MAN="config-files/manager.cfg"
+export NODE=$(kubectl --kubeconfig $1 get pods -n cattle-fleet-system -l app=fleet-agent -o json \
+    | jq '.items | .[0].spec.nodeName')
+touch log.txt
+kubectl --kubeconfig $KUBECONFIG_MAN get clusters.fleet.cattle.io -n clusters > log.txt
+export NODE=$(sed -e 's/^"//' -e 's/"$//' <<< "$NODE")
+IFS=' ' read -ra CLUSTER <<< "$(grep $NODE log.txt)"
+export CLUSTER
+rm log.txt
+kubectl delete clusters.fleet.cattle.io $CLUSTER --kubeconfig $KUBECONFIG_MAN -n clusters
 if [ grep "fleet-agent" log.txt ]
 then
     rm log.txt
--- scripts/update-cluster-ring.sh
+++ scripts/update-cluster-ring.sh
@@ -2,12 +2,12 @@
 export KUBECONFIG_MAN="config-files/manager.cfg"
 export NODE=$(kubectl --kubeconfig $1 get pods -n cattle-fleet-system -l app=fleet-agent -o json \
     | jq '.items | .[0].spec.nodeName')
-touch clusters.txt
+touch log.txt
 kubectl --kubeconfig $KUBECONFIG_MAN get clusters.fleet.cattle.io -n clusters > log.txt
 export NODE=$(sed -e 's/^"//' -e 's/"$//' <<< "$NODE")
 IFS=' ' read -ra CLUSTER <<< "$(grep $NODE log.txt)"
 export CLUSTER
-rm clusters.txt
+rm log.txt
 touch "$CLUSTER.json"
 kubectl --kubeconfig $KUBECONFIG_MAN -n clusters get clusters.fleet.cattle.io $CLUSTER -o json > "$CLUSTER.json"
 sed -i.bak "s/$2/$3/g" "$CLUSTER.json"

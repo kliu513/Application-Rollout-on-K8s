6dac9cc48601b0f7399140c12a0266fdfa11a038
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-02-10 01:03:08 +0800

    Fix

--- scripts/remove-cluster.sh
+++ scripts/remove-cluster.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-touch log.txt
-helm --kubeconfig $1 -n cattle-fleet-system uninstall fleet-agent
-kubectl --kubeconfig $1 get deployments --all-namespaces > log.txt
 export KUBECONFIG_MAN="config-files/manager.cfg"
 export NODE=$(kubectl --kubeconfig $1 get pods -n cattle-fleet-system -l app=fleet-agent -o json \
     | jq '.items | .[0].spec.nodeName')
-touch log.txt
-kubectl --kubeconfig $KUBECONFIG_MAN get clusters.fleet.cattle.io -n clusters > log.txt
+touch clusters.txt
+kubectl --kubeconfig $KUBECONFIG_MAN get clusters.fleet.cattle.io -n clusters > clusters.txt
 export NODE=$(sed -e 's/^"//' -e 's/"$//' <<< "$NODE")
-IFS=' ' read -ra CLUSTER <<< "$(grep $NODE log.txt)"
+IFS=' ' read -ra CLUSTER <<< "$(grep $NODE clusters.txt)"
 export CLUSTER
-rm log.txt
+rm clusters.txt
 kubectl delete clusters.fleet.cattle.io $CLUSTER --kubeconfig $KUBECONFIG_MAN -n clusters
+touch log.txt
+helm --kubeconfig $1 -n cattle-fleet-system uninstall fleet-agent
 if [ grep "fleet-agent" log.txt ]
 then
     rm log.txt

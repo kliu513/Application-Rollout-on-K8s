93513bbd39e843d60baab216e25948dbb0908d50
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-02-09 23:11:10 +0800

    Fix

--- scripts/update-cluster-ring.sh
+++ scripts/update-cluster-ring.sh
@@ -2,17 +2,14 @@
 export KUBECONFIG_MAN="config-files/manager.cfg"
 export NODE=$(kubectl --kubeconfig $1 get pods -n cattle-fleet-system -l app=fleet-agent -o json \
     | jq '.items | .[0].spec.nodeName')
-echo "here"
 touch clusters.txt
-kubectl --kubeconfig $KUBECONFIG_MAN get clusters -n clusters > log.txt
-echo "here"
+kubectl --kubeconfig $KUBECONFIG_MAN get clusters.fleet.cattle.io -n clusters > log.txt
 export NODE=$(sed -e 's/^"//' -e 's/"$//' <<< "$NODE")
-export CLUSTER=$(grep $NODE log.txt)
-echo $CLUSTER
+IFS=' ' read -ra CLUSTER <<< "$(grep $NODE log.txt)"
+export CLUSTER
 rm clusters.txt
 touch cluster.json
-kubectl --kubeconfig $KUBECONFIG_MAN -n clusters get cluster $CLUSTER -o json > cluster.json
-echo "here"
+kubectl --kubeconfig $KUBECONFIG_MAN -n clusters get clusters.fleet.cattle.io $CLUSTER -o json > cluster.json
 sed -i.bak "s/$2/$3/g" cluster.json
 kubectl apply -f cluster.json
 rm cluster.json

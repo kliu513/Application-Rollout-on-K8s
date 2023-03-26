9c1889ecbb372769d7cf6d83683aa5149b0c34d6
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-03-26 23:28:44 +0800

    Test

--- scripts/create-service.sh
+++ scripts/create-service.sh
@@ -3,11 +3,13 @@ export KUBECONFIG_MAN="config-files/manager.cfg"
 touch log.txt
 kubectl --kubeconfig $KUBECONFIG_MAN -n clusters get fleet > log.txt
 n_lines_before=`wc --lines < log.txt`
+echo $n_lines_before
 git clone $1
 cd $2
 kubectl apply -f repo.yaml
 kubectl --kubeconfig $KUBECONFIG_MAN -n clusters get fleet > log.txt
 n_lines_after=`wc --lines < log.txt`
+echo $n_lines_after
 rm log.txt
 if [ $n_lines_after -gt $n_lines_before ]
 then

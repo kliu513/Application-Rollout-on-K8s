58a083a3d5f5639bb7c981d6e133d1e236e51cbe
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-03-27 07:30:23 +0800

    Fix

--- scripts/create-rollout.sh
+++ scripts/create-rollout.sh
@@ -3,7 +3,7 @@ export KUBECONFIG_MAN="config-files/manager.cfg"
 cd $1
 export DIR=$(yq -r .spec.paths[0] repo.yaml)
 cd $DIR
-sed "0,/tag: $2/{s/tag: $2/tag: $3/}" fleet.yaml
+sed -i "0,/tag: $2/{s/tag: $2/tag: $3/}" fleet.yaml
 git add .
 git commit -m "$4 $3"
 git push

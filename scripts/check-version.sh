886b90388ef84543dd996603c3d6dd4c9261588c
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-03-29 01:09:34 +0800

    Test

--- scripts/check-version.sh
+++ scripts/check-version.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 cd $2
 export DIR=$(yq -r .spec.paths[0] repo.yaml)
-cd $DIR
-export NS=$(yq -r .namespace fleet.yaml)
-cd
-export IMAGE=$(kubectl --kubeconfig $1 get deployment -n $NS -o json\
+cd ..
+export IMAGE=$(kubectl --kubeconfig $1 get deployment -n fleet-mc-helm-example -o json\
  | jq '.items | .[0].spec.template.spec.containers | .[0].image')
 export IMAGE=$(sed -e 's/^"//' -e 's/"$//' <<< $IMAGE)
 IFS=':' read -ra version <<< $IMAGE

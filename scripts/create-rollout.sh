7984165a171d8d1481e47d675163501f38f27f8b
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-03-29 09:45:08 +0800

    Fix

--- scripts/create-rollout.sh
+++ scripts/create-rollout.sh
@@ -2,7 +2,7 @@
 cd $1
 export DIR=$(yq -r .spec.paths[0] repo.yaml)
 cd $DIR
-sed -i "/$4/,/tag: $2/{s/tag: $2/tag: $3/}" fleet.yaml
+sed -i "/name: $4/,/tag: $2/{s/tag: $2/tag: $3/}" fleet.yaml
 git add .
 git commit -m "$4 $3"
 git push

fa9752b0d39e6ae9a76573b7f6ae796f0e187cf0
kliu513 <kaitlyn.liu.chang@gmail.com>
2023-03-31 14:34:39 +0800

    Fix

--- scripts/check-version.sh
+++ scripts/check-version.sh
@@ -3,13 +3,12 @@ cd $2
 export DIR=$(yq -r .spec.paths[0] repo.yaml)
 cd $DIR
 export NS=$(yq -r .namespace fleet.yaml)
-cd
-cd App-Rollout-on-K8s
-export IMAGE=$(kubectl --kubeconfig $1 get deployment -n fleet-mc-helm-example -o json\
+cd ~/App-Rollout-on-K8s
+export IMAGE=$(kubectl --kubeconfig $1 get deployment -n $NS -o json\
  | jq '.items | .[0].spec.template.spec.containers | .[0].image')
 export IMAGE=$(sed -e 's/^"//' -e 's/"$//' <<< $IMAGE)
 IFS=':' read -ra version <<< $IMAGE
-echo ${version[-1]}
+echo "Current version: ${version[-1]}"
 if [ ${version[-1]} = $3 ]
 then
     exit 1
